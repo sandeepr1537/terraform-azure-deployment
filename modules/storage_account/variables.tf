f6746e07f2bcfcfd00f137f0374faab6fc547f71
sandeepr1537 <164096457+sandeepr1537@users.noreply.github.com>
2024-09-25 11:51:33 +1200

    Add files via upload

---
+++ modules/storage_account/variables.tf
@@ -0,0 +1,14 @@
+variable "storage_account_name" {
+  description = "Name of the storage account"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure location for the storage account"
+  type        = string
+}
