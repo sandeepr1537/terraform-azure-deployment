615629ba60efd74a64ba495dd8d4f4c02096c48f
sandeepr1537 <164096457+sandeepr1537@users.noreply.github.com>
2024-09-25 13:10:52 +1200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,10 +8,6 @@ variable "location" {
   type        = string
 }
 
-variable "storage_account_name" {
-  description = "Name of the storage account"
-  type        = string
-}
 
 variable "subscription_id" {
   description = "Azure subscription ID"
