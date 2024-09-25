4b2b05565431a26cd9838577ef381c65f533c521
sandeepr1537 <164096457+sandeepr1537@users.noreply.github.com>
2024-09-25 13:09:54 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,6 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.3.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
-
-  # Use environment variables for authentication
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
 }
-
-
 
 # Include the resource group module
 module "resource_group" {
@@ -26,10 +9,3 @@ module "resource_group" {
   location            = var.location
 }
 
-# Include the storage account module
-module "storage_account" {
-  source = "./modules/storage_account"
-  storage_account_name = var.storage_account_name
-  location            = var.location
-  resource_group_name = module.resource_group.resource_group_name
-}
