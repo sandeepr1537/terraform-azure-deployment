d94ec3c05f0983a6fa4adaf8bbbaa7e8c02a56a2
sandeepr1537 <164096457+sandeepr1537@users.noreply.github.com>
2024-09-25 14:19:24 +1200

    Delete modules directory

--- modules/resource_group/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "resource_group_name" {
-  description = "Name of the resource group"
-  type        = string
-}
-
-variable "location" {
-  description = "Azure location for the resource group"
-  type        = string
-}
